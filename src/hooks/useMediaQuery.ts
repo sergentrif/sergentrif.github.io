95a1dee54b3db01abedcaaa1fdc1617adb2e6535
Adrien Blandin <adrien.blandin@gmail.com>
2025-12-24 10:20:47 +0100

    ci(Workflows): add javascript tests

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -7,7 +7,7 @@ export function useMediaQuery(query: string): boolean {
 
     useEffect(() => {
         const media = window.matchMedia(query);
-        
+
         // Set initial value
         setMatches(media.matches);
 
